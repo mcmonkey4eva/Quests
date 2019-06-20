4534f72282fbd1f17ff7b2ef1bcb1bbc53847486
PikaMug <unconfigured@null.spigotmc.org>
2019-06-20 14:19:23 -0400

    Better stage/action item serialization. Convert from legacy on save

--- main/src/main/java/me/blackvein/quests/QuestFactory.java
+++ main/src/main/java/me/blackvein/quests/QuestFactory.java
@@ -795,7 +795,7 @@ public class QuestFactory implements ConversationAbandonedListener {
 		String failMessage = null;
 		Integer moneyRew = null;
 		Integer questPointsRew = null;
-		List<String> itemRews = null;
+		List<ItemStack> itemRews = null;
 		List<Integer> RPGItemRews = null;
 		List<Integer> RPGItemAmounts = null;
 		Integer expRew = null;
@@ -882,10 +882,7 @@ public class QuestFactory implements ConversationAbandonedListener {
 			questPointsRew = (Integer) cc.getSessionData(CK.REW_QUEST_POINTS);
 		}
 		if (cc.getSessionData(CK.REW_ITEMS) != null) {
-			itemRews = new LinkedList<String>();
-			for (ItemStack is : (List<ItemStack>) cc.getSessionData(CK.REW_ITEMS)) {
-				itemRews.add(ItemUtil.serializeItemStack(is));
-			}
+			itemRews = (List<ItemStack>) cc.getSessionData(CK.REW_ITEMS);
 		}
 		if (cc.getSessionData(CK.REW_EXP) != null) {
 			expRew = (Integer) cc.getSessionData(CK.REW_EXP);
@@ -949,16 +946,10 @@ public class QuestFactory implements ConversationAbandonedListener {
 		cs.set("finish-message", finish);
 		cs.set("event", initialEvent);
 		cs.set("region", region);
-		cs.set("gui-display", ItemUtil.serializeItemStack(guiDisplay));
+		cs.set("gui-display", guiDisplay);
 		if (moneyReq != null || questPointsReq != null || itemReqs != null && itemReqs.isEmpty() == false || permReqs != null && permReqs.isEmpty() == false || (questReqs != null && questReqs.isEmpty() == false) || (questBlocks != null && questBlocks.isEmpty() == false) || (mcMMOSkillReqs != null && mcMMOSkillReqs.isEmpty() == false) || heroesPrimaryReq != null || heroesSecondaryReq != null || customReqs != null) {
 			ConfigurationSection reqs = cs.createSection("requirements");
-			List<String> items = new LinkedList<String>();
-			if (itemReqs != null) {
-				for (ItemStack is : itemReqs) {
-					items.add(ItemUtil.serializeItemStack(is));
-				}
-			}
-			reqs.set("items", (items.isEmpty() == false) ? items : null);
+			reqs.set("items", itemReqs);
 			reqs.set("remove-items", removeItemReqs);
 			reqs.set("money", moneyReq);
 			reqs.set("quest-points", questPointsReq);
@@ -999,7 +990,6 @@ public class QuestFactory implements ConversationAbandonedListener {
 		LinkedList<Integer> cutIds;
 		LinkedList<Integer> cutAmounts;
 		LinkedList<Short> cutDurability;
-		LinkedList<ItemStack> deliveryItems;
 		LinkedList<String> enchantments;
 		LinkedList<Integer> enchantmentIds;
 		LinkedList<Integer> enchantmentAmounts;
@@ -1007,6 +997,7 @@ public class QuestFactory implements ConversationAbandonedListener {
 		Integer players;
 		LinkedList<ItemStack> craftItems;
 		LinkedList<ItemStack> smeltItems;
+		LinkedList<ItemStack> deliveryItems;
 		LinkedList<Integer> deliveryNPCIds;
 		LinkedList<String> deliveryMessages;
 		LinkedList<Integer> npcTalkIds;
@@ -1252,20 +1243,12 @@ public class QuestFactory implements ConversationAbandonedListener {
 				stage.set("cut-block-durability", cutDurability);
 			}
 			if (craftItems != null && craftItems.isEmpty() == false) {
-				LinkedList<String> items = new LinkedList<String>();
-				for (ItemStack is : craftItems) {
-					items.add(ItemUtil.serializeItemStack(is));
-				}
-				stage.set("items-to-craft", items);
+				stage.set("items-to-craft", craftItems);
 			} else {
 				stage.set("items-to-craft", null);
 			}
 			if (smeltItems != null && smeltItems.isEmpty() == false) {
-				LinkedList<String> items = new LinkedList<String>();
-				for (ItemStack is : smeltItems) {
-					items.add(ItemUtil.serializeItemStack(is));
-				}
-				stage.set("items-to-smelt", items);
+				stage.set("items-to-smelt", smeltItems);
 			} else {
 				stage.set("items-to-smelt", null);
 			}
@@ -1275,11 +1258,7 @@ public class QuestFactory implements ConversationAbandonedListener {
 			stage.set("fish-to-catch", fish);
 			stage.set("players-to-kill", players);
 			if (deliveryItems != null && deliveryItems.isEmpty() == false) {
-				LinkedList<String> items = new LinkedList<String>();
-				for (ItemStack is : deliveryItems) {
-					items.add(ItemUtil.serializeItemStack(is));
-				}
-				stage.set("items-to-deliver", items);
+				stage.set("items-to-deliver", deliveryItems);
 			} else {
 				stage.set("items-to-deliver", null);
 			}
